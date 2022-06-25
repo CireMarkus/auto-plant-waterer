58ab690e7d19bcbf6e08645576f920ba420898fd
Eric Washington <mrkimjarrow95@gmail.com>
2022-06-24 21:53:02 -0500

    Created the script for the moisture sensor class

---
+++ MoistureSensor.py
@@ -0,0 +1,10 @@
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+class MoistureSensor:
+    def __init__(self) -> None:
+        pass
