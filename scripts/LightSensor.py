fb0730a9d1f1db4bfebd357127943b8e682a5d68
Cire_Markus <mrkimjarrow95@gmail.com>
2023-03-12 01:25:52 -0600

    Started working on the light sensor class.

---
+++ scripts/LightSensor.py
@@ -0,0 +1,31 @@
+import board
+import adafruit_veml7700
+
+class LightSensor: 
+    
+    __i2c = None
+    __veml7700 = None
+    __gain = None
+    __iTime = None
+    
+    def __init__(self) -> None:
+        try:
+            self.__i2c = board.I2C() #uses board.SCL and board.SDA
+            self.__veml7700 = adafruit_veml7700.VEML7700(self.__i2c)
+            self.__gain = self.__veml7700.ALS_GAIN_1_8
+            self.__iTime = self.__veml7700.ALS_25MS
+            self.__veml7700.light_integration_time =self.__iTime
+            self.__veml7700.light_gain = self.__gain
+        except Exception as e: 
+            print("The following error has occured during initialization: {}".format(e))
+    
+    #returns the current light intensity with lux first and voltage second
+    def getCurrentLight(self):
+        return (self.__veml7700.lux,self.__veml7700.light)
+    
+
+if __name__ == "__main__":
+    light = LightSensor()
+    while(True):
+        vals = LightSensor.getCurrentLight()
+        print("Current Lux value: {} Current light value: {}".format(vals[0],vals[1]), end='\r')
