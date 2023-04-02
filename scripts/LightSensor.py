9bea383b9dbc1471d8dfd689bac51516b0009c58
Cire_Markus <mrkimjarrow95@gmail.com>
2023-04-01 23:33:37 -0500

    created place holder for autoAdjust function

--- scripts/LightSensor.py
+++ scripts/LightSensor.py
@@ -4,24 +4,28 @@ import adafruit_veml7700
 class LightSensor: 
     
     __i2c = None
-    __veml7700 = None
+    __sensor = None
     __gain = None
     __iTime = None
     
     def __init__(self) -> None:
         try:
             self.__i2c = board.I2C() #uses board.SCL and board.SDA
-            self.__veml7700 = adafruit_veml7700.VEML7700(self.__i2c)
-            self.__gain = self.__veml7700.ALS_GAIN_1_8
-            self.__iTime = self.__veml7700.ALS_25MS
-            self.__veml7700.light_integration_time =self.__iTime
-            self.__veml7700.light_gain = self.__gain
+            self.__sensor = adafruit_veml7700.VEML7700(self.__i2c)
+            self.__gain = self.__sensor.ALS_GAIN_1_8 #gain of the sensor
+            self.__iTime = self.__sensor.ALS_25MS #integration time
+            self.__sensor.light_integration_time =self.__iTime
+            self.__sensor.light_gain = self.__gain
         except Exception as e: 
             print("The following error has occured during initialization: {}".format(e))
     
     #returns the current light intensity with lux first and voltage second
     def getCurrentLight(self):
-        return (self.__veml7700.lux,self.__veml7700.light)
+        return (self.__sensor.lux,self.__sensor.light)
+    
+    #TODO write function that will automatically adjust the sensitivity of the sensor as the amibient light changes.
+    def autoAdjust(self):
+        pass
     
 
 if __name__ == "__main__":
