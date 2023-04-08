64ceea037768e77b2b852cfffd6db6f9301852c8
Cire_Markus <mrkimjarrow95@gmail.com>
2023-04-08 14:40:20 -0500

    having the runs counted while being processed.

--- scripts/temphumsensortest.py
+++ scripts/temphumsensortest.py
@@ -13,10 +13,12 @@ while runs < 1000000:
     try:
         temp = dht.temperature * (9/5) +32
         hum = dht.humidity
-        print("Temperature(F): {}, Humidity: {}".format(temp,hum))
+        #print("Temperature(F): {}, Humidity: {}".format(temp,hum))
     except Exception as error:
-        print(error)
+        #print(error)
+        pass
     runs += 1
+    print(runs,end='\r')
     
 print ("Runs: {}, Successful Runs: {} {}%, Failed Runs: {} {}%".format(runs,successfulRuns,((successfulRuns/runs)*100),failedRuns,((failedRuns/runs)*100)))
     
