59714458f17ca092739444254a18f2c7fff47a67
Cire_Markus <mrkimjarrow95@gmail.com>
2023-04-08 18:15:05 -0500

    running 100k and logging the exceptions so I can catch them.

--- scripts/temphumsensortest.py
+++ scripts/temphumsensortest.py
@@ -7,8 +7,10 @@ dht = adafruit_dht.DHT22(board.D26)
 runs = 0; 
 successfulRuns = 0;
 failedRuns = 0;
+errors = []
 
-while runs < 10000:
+
+while runs < 100000:
     
     try:
         temp = dht.temperature * (9/5) +32
@@ -16,12 +18,10 @@ while runs < 10000:
         print("Temperature(F): {}, Humidity: {}".format(temp,hum))
         successfulRuns += 1
     except Exception as error:
-        print(error)
-        failedRuns += 1
-        pass
-    runs += 1
-    print(runs,end='\r')
+        errors.append(error)
+ 
     
-print ("Runs: {}, Successful Runs: {} {}%, Failed Runs: {} {}%".format(runs,successfulRuns,((successfulRuns/runs)*100),failedRuns,((failedRuns/runs)*100)))
+for i in errors:
+    print(i)
     
     
