dfeea3a6f60eb050be9f254f8899e4d4756aa67c
CireMarkus <mrkimjarrow95@gmail.com>
2025-05-13 19:30:42 -0500

    Enhance sensor test output with detailed run status messages

--- scripts/temphumsensortest.py
+++ scripts/temphumsensortest.py
@@ -11,13 +11,16 @@ errors = []
 
 
 while runs < 100:
+    print("Run: {}".format(runs))
     time.sleep(3)
     try:
+        print("Attempting to read DHT22 sensor")
         temp = dht.temperature * (9/5) +32
         hum = dht.humidity
         print("Temperature(F): {}, Humidity: {}".format(temp,hum))
         successfulRuns += 1
     except Exception as error:
+        print("Failed to read DHT22 sensor")
         errors.append(error)
     runs+=1
 
