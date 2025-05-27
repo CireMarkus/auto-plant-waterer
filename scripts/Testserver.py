bf9bdbff425b76fe23d801c9db9a35be486318a1
CireMarkus <mrkimjarrow95@gmail.com>
2025-05-26 22:59:17 -0500

    Refactor run function to handle trigger wait based on threading or asyncio and update characteristic values directly

--- scripts/Testserver.py
+++ scripts/Testserver.py
@@ -94,16 +94,19 @@ async def run(loop):
     await server.start()
     logger.debug("Advertising")
     print("Advertising started. Press Ctrl+C to stop.")
-   
+    if trigger.__module__ == "threading":
+        trigger.wait()
+    else:
+        await trigger.wait()
     
-    
+    await asyncio.sleep(2)
     logger.debug("Updating")
     print("Updating values...")
-    server.get_characteristic(temp_char_uuid)
+    server.get_characteristic(temp_char_uuid).value(temp_test_data().to_bytes(2, 'little'))
     server.update_value(service_uuid, temp_char_uuid)
-    server.get_characteristic(hum_char_uuid)
+    server.get_characteristic(hum_char_uuid).value(hum_test_data().to_bytes(2, 'little'))
     server.update_value(service_uuid, hum_char_uuid)
-    
+    await asyncio.sleep(2)
     trigger.wait()
     
     
