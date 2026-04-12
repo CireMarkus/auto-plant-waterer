31e015b1e4dd7f161dad6e22b77b3860874be675
CireMarkus <mrkimjarrow95@gmail.com>
2026-04-12 18:11:18 -0500

    Created a constant file to hold config data.

---
+++ common/ConfigConst.py
@@ -0,0 +1,23 @@
+"""
+Configuration constants and other default values.
+"""
+
+
+NOT_SET = 'Not Set'
+DEFAULT_SENSOR_TYPE = 0
+
+#System Util Constants
+SYSTEM_PERF_TYPE          = 9000
+CPU_UTIL_TYPE             = 9001
+DISK_UTIL_TYPE            = 9002
+MEM_UTIL_TYPE             = 9003
+NET_IN_UTIL_TYPE          = 9004
+NET_OUT_UTIL_TYPE         = 9005
+
+CPU_UTIL_NAME     = 'DeviceCpuUtil'
+DISK_UTIL_NAME    = 'DeviceDiskUtil'
+MEM_UTIL_NAME     = 'DeviceMemUtil'
+NET_IN_UTIL_NAME  = 'DeviceNetInUtil'
+NET_OUT_UTIL_NAME = 'DeviceNetOutUtil'
+
+#Sensor Constants
