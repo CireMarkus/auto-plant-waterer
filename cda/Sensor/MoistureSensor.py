165003678bad63f97e7f3cd939bcae59e4e001ed
ciremarkus <mrkimjarrow95@gmail.com>
2026-04-27 21:17:33 -0500

    created a config file to hold sensor values and added config function to the moisture sensor.

--- cda/Sensor/MoistureSensor.py
+++ cda/Sensor/MoistureSensor.py
@@ -4,6 +4,7 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP 
 from adafruit_mcp3xxx.analog_in import AnalogIn
+import json
 
 import common.ConfigConst as ConfigConst
 from cda.Sensor.BaseSensor import BaseSensor
@@ -12,8 +13,16 @@ class MoistureSensor(BaseSensor):
 
     def __init__(self, name, typeID, floor=None, ceiling=None):
         super().__init__(name, typeID, floor, ceiling)
-        self.spi = buiso.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-        self.cs  = digitalio.DigitalInOut(board.D8)
-        self.mcp = MCP.MCP3008(self.spi,self.cs)
-        self.channel = AnalogIn(mcp,MCP.p0)
+        try:
+            self.spi = buiso.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+            self.cs  = digitalio.DigitalInOut(board.D8)
+            self.mcp = MCP.MCP3008(self.spi,self.cs)
+            self.channel = AnalogIn(mcp,MCP.P0)
+            
+        except Exception as e: 
+            logging.error(f"The following error occurred while initializing {e}")
         
+
+    #TODO: This function shall log the relative min and compare it to the absolute min. 
+    def _calibrate(self):
+        pass
