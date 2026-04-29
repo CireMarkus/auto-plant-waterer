11f715e2bbfa07660303c89d0b4b53eb58d9ddd3
CireMarkus <mrkimjarrow95@gmail.com>
2026-04-29 01:37:43 -0500

    Update key values.

--- tests/unit/Actuator/test_LED.py
+++ tests/unit/Actuator/test_LED.py
@@ -14,30 +14,30 @@ class LedTest(unittest.TestCase):
         logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
         logging.info("Initializing LED Unit Test...")
         # Calibration: Wet=24000 (Ceiling), Dry=52000 (Floor)
-        cls.led_display = LED(wet_value=24000, dry_value=52000)
+        cls.led = LED(floor=24000, ceiling=52000)
     
     def testColorTransitions(self): 
         # Test 1: Simulate Bone Dry (Should be Red)
         logging.info("Simulating DRY soil...")
-        self.led_display.updateLedColor(52000)
-        self.assertAlmostEqual(self.led_display.moisture_perc, 0.0)
+        self.led.updateLedColor(52000)
+        self.assertAlmostEqual(self.led.moisture_perc, 0.0)
         time.sleep(1)
 
         # Test 2: Simulate Ideal Moisture (Should be Green)
         logging.info("Simulating PERFECT soil...")
-        self.led_display.updateLedColor(38000)
-        self.assertTrue(0.4 < self.led_display.moisture_perc < 0.6)
+        self.led.updateLedColor(38000)
+        self.assertTrue(0.4 < self.led.moisture_perc < 0.6)
         time.sleep(1)
 
         # Test 3: Simulate Submerged (Should be Blue)
         logging.info("Simulating WET soil...")
-        self.led_display.updateLedColor(24000)
-        self.assertAlmostEqual(self.led_display.moisture_perc, 1.0)
+        self.led.updateLedColor(24000)
+        self.assertAlmostEqual(self.led.moisture_perc, 1.0)
         time.sleep(1)
 
     def testSweep(self):
         """Watch the LED smoothly transition from Red to Blue"""
         logging.info("Running smooth color sweep test...")
         for val in range(52000, 24000, -1000):
-            self.led_display.updateLedColor(val)
+            self.led.updateLedColor(val)
             time.sleep(0.1)
