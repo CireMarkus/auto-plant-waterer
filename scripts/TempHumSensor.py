c878a142ab59af05450962ec679ce5db66a302f1
CireMarkus <mrkimjarrow95@gmail.com>
2026-05-16 12:11:17 -0500

    Refactor TempHumSensor to use AHTx0 and simplify test script

--- scripts/TempHumSensor.py
+++ scripts/TempHumSensor.py
@@ -1,20 +1,18 @@
 import time
 import board
-import adafruit_dht
+import adafruit_ahtx0
 
 class TempHumSensor: 
-    __dht = adafruit_dht.DHT22(board.D26)
+    _sensor = adafruit_ahtx0.AHTx0(board.I2C())
     
     def __init__(self):
         pass
     
     
     def getTempFarenheit(self):
-        time.sleep(2.5)
-        return self.__dht.temp *(9.0/5.0) + 32.0
+        return self._sensor.temperature *(9.0/5.0) + 32.0
     
     def getHumidity(self):
-        time.sleep(2.5)
-        return self.__dht.humidity
+        return self._sensor.relative_humidity
     
         
--- scripts/temphumsensortest.py
+++ scripts/temphumsensortest.py
@@ -1,33 +1,8 @@
-import time
-import board
-import adafruit_dht
-
-dht = adafruit_dht.DHT22(board.D26)
-
-runs = 0; 
-successfulRuns = 0;
-failedRuns = 0;
-errors = []
+from TempHumSensor import TempHumSensor
 
 
-while runs < 10:
-    print("Run: {}".format(runs))
-    
-    try:
-        print("Attempting to read DHT22 sensor")
-        time.sleep(2)
-        temp = dht.temperature * (9/5) +32
-        time.sleep(2)
-        hum = dht.humidity
-        print("Temperature(F): {}, Humidity: {}".format(temp,hum))
-        successfulRuns += 1
-    except Exception as error:
-        print("Failed to read DHT22 sensor")
-        errors.append(error)
-    runs+=1
+sensor = TempHumSensor()
+while True:
+    print(f"Temp: {sensor.getTempFarenheit():.2f}\n\n")
+    print(f"Humidity: {sensor.getHumidity():.2f}\n\n")
 
-print("\n\n\n\n\n\n\n\n\n") 
-for i in errors:
-    print(i)
-    
-    
