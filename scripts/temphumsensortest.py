d436a9e8eab337fab57500976830ea8a0bc0fc4c
CireMarkus <mrkimjarrow95@gmail.com>
2026-05-16 12:19:16 -0500

    Squashed commit of the following:
    commit 67d83aa2ba7567858ac7facf8fa08e16a398f5c9
    Author: CireMarkus <mrkimjarrow95@gmail.com>
    Date:   Sat May 16 12:12:16 2026 -0500
    
        Remove unused time import from TempHumSensor and test script
    
    commit c878a142ab59af05450962ec679ce5db66a302f1
    Author: CireMarkus <mrkimjarrow95@gmail.com>
    Date:   Sat May 16 12:11:17 2026 -0500
    
        Refactor TempHumSensor to use AHTx0 and simplify test script

--- scripts/temphumsensortest.py
+++ scripts/temphumsensortest.py
@@ -1,33 +1,8 @@
+from TempHumSensor import TempHumSensor
 import time
-import board
-import adafruit_dht
 
-dht = adafruit_dht.DHT22(board.D26)
-
-runs = 0; 
-successfulRuns = 0;
-failedRuns = 0;
-errors = []
-
-
-while runs < 10:
-    print("Run: {}".format(runs))
-    
-    try:
-        print("Attempting to read DHT22 sensor")
-        time.sleep(2)
-        temp = dht.temperature * (9/5) +32
-        time.sleep(2)
-        hum = dht.humidity
-        print("Temperature(F): {}, Humidity: {}".format(temp,hum))
-        successfulRuns += 1
-    except Exception as error:
-        print("Failed to read DHT22 sensor")
-        errors.append(error)
-    runs+=1
-
-print("\n\n\n\n\n\n\n\n\n") 
-for i in errors:
-    print(i)
-    
-    
+sensor = TempHumSensor()
+while True:
+    print(f"Temp: {sensor.getTempFarenheit():.2f}\n\n")
+    print(f"Humidity: {sensor.getHumidity():.2f}\n\n")
+    time.sleep(2)
