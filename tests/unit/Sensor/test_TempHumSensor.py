5c9d89aa07c89bf885890f6318a08bcf34bb4734
CireMarkus <mrkimjarrow95@gmail.com>
2026-05-19 18:18:52 -0500

    Bare bones unit test for temp humidity sensor testing.

---
+++ tests/unit/Sensor/test_TempHumSensor.py
@@ -0,0 +1,35 @@
+import logging
+import unittest
+import time
+
+from cda.Sensor.TempHumSensor import TempHumSensor
+
+class MoistureSensorTest(unittest.TestCase):
+    """
+    Validate the functionality of the cpu utilization task. 
+    """
+    
+    @classmethod
+    def setUpClass(self):
+        logging.basicConfig(format = '%(asctime)s:%(module)s:%(levelname)s:%(message)s', level = logging.DEBUG)
+        logging.info("Testing TempSensor class...")
+        self.sensorName = 'TempSensor'
+        self.sensorType = '1'
+        self.sensor = TempHumSensor(self.sensorName,self.sensorType)
+    
+    def testGetName(self):
+        self.assertEqual(self.sensor.getName(),self.sensorName)
+
+    def testGetType(self):
+        self.assertEqual(self.sensor.getTypeID(),self.sensorType)
+
+    def testGetTelemetryValue(self):        
+        for i in range(0,20):
+            val = self.sensor.getTelemetry()
+            temperature = val[0]
+            humidity = val[1]
+            logging.info(f" temp: {temperature} C, humidity: {humidity}")
+            self.assertGreater(temperature,0)
+            self.assertGreater(humidity,0)
+            time.sleep(1)
+        
